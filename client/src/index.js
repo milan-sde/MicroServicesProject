c275f84233c7a796bdd13cf7383320d7d9f2593a
milan-sde <25mca037@nirmauni.ac.in>
2026-04-09 13:38:47 +0530

    dockfile

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div>
+      <h1>Smart Order Management System</h1>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
